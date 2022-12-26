ea64f686bdd53dff1c9c1e8933eb61d7d16e6a40
chia7812033 <dan891221@gmail.com>
2022-12-26 22:36:39 +0800

    Everything fine

---
+++ members-only/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :posts, only: [:new, :create, :index]
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "posts#index"
+end
