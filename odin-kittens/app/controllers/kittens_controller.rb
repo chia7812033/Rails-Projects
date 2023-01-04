1f6681f8ff52c6f224150160ced9227ef9a15fe8
chia7812033 <dan891221@gmail.com>
2023-01-04 20:30:10 +0800

    Finish api

--- odin-kittens/app/controllers/kittens_controller.rb
+++ odin-kittens/app/controllers/kittens_controller.rb
@@ -1,10 +1,20 @@
 class KittensController < ApplicationController
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html {}
+      format.json { render :json => @kittens }
+    end
   end
 
   def show
     @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html {}  
+      format.json { render :json => @kitten }
+    end
   end
 
   def new
@@ -14,9 +24,35 @@ class KittensController < ApplicationController
   def create
     @kitten = Kitten.new(kitten_params)
     if @kitten.save
-      redirect_to @kitten
+      redirect_to @kitten, notice: "You create a new kitten!"
     else
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to @kitten, notice: "You edit the kitten successfully!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    redirect_to root_path, status: :see_other, notice: "The kitten is deleted"
+  end
+
+  private
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
 end
